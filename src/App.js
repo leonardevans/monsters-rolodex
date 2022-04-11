7b3dc6de5bb5677d7d7adbabd911e58c52669824
leonardevans <lemutugi@gmail.com>
2022-04-11 05:38:48 -0400

    Storing Original Data

--- src/App.js
+++ src/App.js
@@ -3,23 +3,51 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component{
+  constructor(){
+    super();
+
+    this.state ={
+      monsters:[],
+      searchField: ''
+    }
+  }
+
+  componentDidMount(){
+    fetch('http://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState(
+      ()=>{
+        return {monsters: users}
+      },
+      ()=>{
+        console.log(this.state);
+      }
+    ))
+  }
+
   render(){
+    const filteredMonsters = this.state.monsters.filter(
+      (monster)=>{
+        return monster.name.toLowerCase().includes(this.state.searchField)
+      }
+    );
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <input className='search-box' type='search' placeholder='search monsters' 
+      onChange={(event)=>{
+        const searchField = event.target.value.toLowerCase();
+
+        this.setState(()=>{
+          return {searchField}
+        })
+      }}
+      />
+        {
+          filteredMonsters.map(monster =>{
+            return <h1 key={monster.id}>{monster.name}</h1>;
+          })
+        }
       </div>
     );
   }
