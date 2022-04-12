1e276621a23280210c7c2a7e725ad515f2b2da55
leonardevans <lemutugi@gmail.com>
2022-04-12 01:32:31 -0400

    convert SearchBox class component to functional component

--- src/components/card-list/card/card.component.jsx
+++ src/components/card-list/card/card.component.jsx
@@ -1,9 +1,7 @@
-import { Component } from "react";
+// import { Component } from "react";
 import './card.styles.css';
 
-class Card extends Component{
-    render(){
-        const {id, name, email} = this.props.monster;
+const Card = ({monster: {id, name, email}})=>{
 
         return(
             <div className="card-container">
@@ -12,7 +10,20 @@ class Card extends Component{
                 <p>{email}</p>
             </div>
         )
-    }
 }
+
+// class Card extends Component{
+//     render(){
+//         const {id, name, email} = this.props.monster;
+
+//         return(
+//             <div className="card-container">
+//                 <img alt={name} src={`https://robohash.org/${id}?set=set2&size=180x180`}/>
+//                 <h2>{name}</h2>
+//                 <p>{email}</p>
+//             </div>
+//         )
+//     }
+// }
 
 export default Card;
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,17 +1,28 @@
-import { Component } from "react";
+// import { Component } from "react";
 import './search-box.style.css'
 
-class SearchBox extends Component{
-    render() {
+const SearchBox = ({className, placeholder, onChangeHandler})=>{
         return (
             <input 
-            className= {`search-box ${this.props.className}`}
+            className= {`search-box ${className}`}
             type='search' 
-            placeholder={this.props.placeholder}
-            onChange={this.props.onChangeHandler}
+            placeholder={placeholder}
+            onChange={onChangeHandler }
       />
         );
-    }
 }
+
+// class SearchBox extends Component{
+//     render() {
+//         return (
+//             <input 
+//             className= {`search-box ${this.props.className}`}
+//             type='search' 
+//             placeholder={this.props.placeholder}
+//             onChange={this.props.onChangeHandler}
+//       />
+//         );
+//     }
+// }
 
 export default SearchBox; 
